a3fa189c62183b29c4405e6d2038e9607abfdf89
Suraj <thesurajsite@gmail.com>
2023-10-07 20:59:14 +0530

    Add contacts Completed

--- app/src/main/java/com/example/contactsapp/ContactModel.java
+++ app/src/main/java/com/example/contactsapp/ContactModel.java
@@ -2,11 +2,19 @@ package com.example.contactsapp;
 
 public class ContactModel {
     int img;
-    String name,number;
-    public ContactModel(int img, String name, String number){
-        this.img=img;
-        this.name=name;
-        this.number=number;
+    String name, number;
+
+    public ContactModel(int img, String name, String number) {
+        this.img = img;
+        this.name = name;
+        this.number = number;
+
+    }
+
+    public ContactModel(String name, String number) {
+        this.name = name;
+        this.number = number;
+
 
     }
 }
--- app/src/main/java/com/example/contactsapp/MainActivity.java
+++ app/src/main/java/com/example/contactsapp/MainActivity.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<ContactModel> arrContact=new ArrayList<>();
+    FloatingActionButton floatingActionButton;
+    RecyclerContactAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,49 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView recyclerview=findViewById(R.id.recyclercontact);
         recyclerview.setLayoutManager(new LinearLayoutManager(this));
+
+        floatingActionButton=findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               // Toast.makeText(MainActivity.this, "Clicked On add", Toast.LENGTH_SHORT).show();
+                Dialog dialog=new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.add_update_layout);
+
+                EditText addName=dialog.findViewById(R.id.addName);
+                EditText addNumber=dialog.findViewById(R.id.addNumber);
+                Button saveButton=dialog.findViewById(R.id.saveButton);
+
+                saveButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String name="",number="";
+                        if(!addName.getText().toString().equals("")) {
+                            name = addName.getText().toString();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Please Enter Name", Toast.LENGTH_SHORT).show();
+                        }
+                        if(!addNumber.getText().toString().equals("")){
+                            number = addNumber.getText().toString();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Please Enter Number", Toast.LENGTH_SHORT).show();
+                        }
+
+                        arrContact.add(new ContactModel(R.drawable.contactimage,name, number));
+                        adapter.notifyItemInserted(arrContact.size()-1);
+                        recyclerview.scrollToPosition(arrContact.size()-1);
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+
+            }
+        });
+
+
 
         arrContact.add(new ContactModel(R.drawable.contactimage , "Suraj Verma", "7667484399"));
         arrContact.add(new ContactModel(R.drawable.contactimage,"Ravi Shankar", "9835168731"));
@@ -35,7 +87,11 @@ public class MainActivity extends AppCompatActivity {
         arrContact.add(new ContactModel(R.drawable.contactimage,"Ankesh IIIT CSE", "7849968913"));
         arrContact.add(new ContactModel(R.drawable.contactimage, "Dheeraj Civil","9829393515"));
 
-        RecyclerContactAdapter adapter=new RecyclerContactAdapter(this, arrContact);
+
+
+
+
+        adapter=new RecyclerContactAdapter(this, arrContact);
         recyclerview.setAdapter(adapter);
 
     }
