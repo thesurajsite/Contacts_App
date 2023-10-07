95ca7db886a804e38ecd707c096496d72e2858cf
Suraj <thesurajsite@gmail.com>
2023-10-07 22:20:47 +0530

    Edit And Delete Button Completed

--- app/src/main/java/com/example/contactsapp/RecyclerContactAdapter.java
+++ app/src/main/java/com/example/contactsapp/RecyclerContactAdapter.java
@@ -1,12 +1,18 @@
 package com.example.contactsapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,6 +41,72 @@ public class RecyclerContactAdapter extends RecyclerView.Adapter<RecyclerContact
         holder.txtName.setText(arrContact.get(position).name);
         holder.txtNumber.setText(arrContact.get(position).number);
 
+        holder.editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog=new Dialog(context);
+                dialog.setContentView(R.layout.add_update_layout);
+
+                TextView addContactTitle=dialog.findViewById(R.id.addContactTitle);
+                EditText addName=dialog.findViewById(R.id.addName);
+                EditText addNumber=dialog.findViewById(R.id.addNumber);
+                Button saveButton=dialog.findViewById(R.id.saveButton);
+
+                addContactTitle.setText("Update contact");
+                addName.setText(arrContact.get(position).name);
+                addNumber.setText(arrContact.get(position).number);
+                saveButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String name="",number="";
+                        if(!addName.getText().toString().equals("")) {
+                            name = addName.getText().toString();
+                        }
+                        else{
+                            Toast.makeText(context, "Please Enter Name", Toast.LENGTH_SHORT).show();
+                        }
+                        if(!addNumber.getText().toString().equals("")){
+                            number = addNumber.getText().toString();
+                        }
+                        else{
+                            Toast.makeText(context, "Please Enter Number", Toast.LENGTH_SHORT).show();
+                        }
+
+                        arrContact.set(position, new ContactModel(R.drawable.contactimage,name,number));
+                        notifyItemChanged(position);
+                        dialog.dismiss();
+
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+
+        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(context)
+                        .setTitle("Delete Contact")
+                        .setIcon(R.drawable.baseline_delete_24)
+                        .setMessage("Do you want to Delete this contact ?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                arrContact.remove(position);
+                                notifyItemRemoved(position);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        });
+                builder.show();
+            }
+        });
+
     }
 
     @Override
@@ -45,12 +117,16 @@ public class RecyclerContactAdapter extends RecyclerView.Adapter<RecyclerContact
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView txtName,txtNumber;
         ImageView imgContact;
+        ImageView editButton, deleteButton;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtName=itemView.findViewById(R.id.txtName);
             txtNumber=itemView.findViewById(R.id.txtNumber);
             imgContact=itemView.findViewById(R.id.imgcontact);
+            editButton=itemView.findViewById(R.id.editButton);
+            deleteButton=itemView.findViewById(R.id.deleteButton);
 
 
         }
